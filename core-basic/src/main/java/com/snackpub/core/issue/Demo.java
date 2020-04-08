c1be70ac08b76f13d4270ea0344eefb89753b2d6
snackpub <1498851306@qq.com>
2020-04-08 16:22:15 +0800

    提交数据处理issue

---
+++ core-basic/src/main/java/com/snackpub/core/issue/Demo.java
@@ -0,0 +1,63 @@
+package com.snackpub.core.issue;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Demo {
+
+    /**
+     * 数据处理题：
+     * 用C或Java或JavaScript语言写一个方法，入参为N*4的二维数组（
+     * 第一列学生学号第二列数学成绩第三列物理成绩第四列化学成绩），返回值为N*5的二维数组（前四列相同，第五列为总分列），要求返回的二维数组按照总分列倒序。
+     */
+    public static void main(String[] args) {
+        String[][] scoreArr = new String[][]{{"学号", "数学", "化学", "生物"}, {"1", "200", "100", "600"}, {"2", "400", "100", "100"}};
+        calculateScore(scoreArr);
+    }
+
+    public static void calculateScore(String[][] arr) {
+        final String[][] newArr = new String[arr.length][5];
+        for (int i = 0; i < arr.length; i++) {
+            int totalScore = 0;
+            for (int i1 = 0; i1 < arr[i].length; i1++) {
+                newArr[i][i1] = arr[i][i1];
+                // 表头，并给数组最后节点追加元素
+                if (i == 0 && i1 == arr[i].length - 1) {
+                    newArr[i][newArr[i].length - 1] = "总分";
+                }
+                // 非数组第一个元素，非第一行；计算总分
+                if (i1 != 0 && i != 0) {
+                    int score = Integer.parseInt(arr[i][i1]);
+                    totalScore += score;
+                }
+                // 将总分追加到数组尾部
+                if (i1 == arr[i].length - 1 && i != 0) {
+                    newArr[i][newArr[i].length - 1] = totalScore + "";
+                }
+            }
+        }
+
+        Integer[][] sortScore = new Integer[arr.length - 1][5];
+        for (int i = 0; i < newArr.length - 1; i++) {
+            for (int i1 = 0; i1 < newArr[i].length; i1++) {
+                if (newArr[i][i1] != null && i != 0) {
+                    sortScore[i][i1] = Integer.parseInt(newArr[i][i1]);
+                }
+            }
+        }
+
+//        Arrays.sort(sortScore, Collections.reverseOrder());
+//        Arrays.sort(sortScore, 3, 3, new Comparator<String[]>() {
+//            @Override
+//            public int compare(String[] o1, String[] o2) {
+//                return Integer.parseInt(o1[0]) - Integer.parseInt(o2[0]);
+//            }
+//        });
+        List<Integer[]> strings = Arrays.asList(sortScore);
+        for (Integer[] string : strings) {
+            for (Integer s : string) {
+                System.out.println(s);
+            }
+        }
+    }
+}
