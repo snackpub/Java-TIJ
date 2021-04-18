d9b10695bb0c134822b170be00e93c4e06740c26
snackpub <1498851306@qq.com>
2021-04-18 14:47:52 +0800

    修改reflect.md

---
+++ core-basic/src/main/java/com/snackpub/core/test/CastingNumbers.java
@@ -0,0 +1,17 @@
+package com.snackpub.core.test;
+
+public class CastingNumbers {
+  public static void main(String[] args) {
+    double above = 0.7, below = 0.4;
+    float fabove = 0.7f, fbelow = 0.4f;
+    System.out.println("(int)above: " + (int)above);
+    System.out.println("(int)below: " + (int)below);
+    System.out.println("(int)fabove: " + (int)fabove);
+    System.out.println("(int)fbelow: " + (int)fbelow);
+  }
+} /* Output:
+(int)above: 0
+(int)below: 0
+(int)fabove: 0
+(int)fbelow: 0
+*///:~
